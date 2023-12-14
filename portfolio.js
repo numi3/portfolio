9ad010b0316eca76c6f9cd98c65bdc2931aea8f2
numi3 <numiyemin1@gmail.com>
2023-12-14 22:19:19 +0200

    Added Page2, Added text, Added smooth transition.

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,22 @@
 // "Hacker effect" code (heavily) inspired by HYPERPLEXED - https://www.youtube.com/watch?v=W5oawMJaXbU
+const welcome = document.querySelector("#welcome");
 document.addEventListener('DOMContentLoaded', () => {
-    const welcome = document.querySelector("#welcome");
     welcome.innerHTML = welcome.dataset.value;
     hackerEffect(welcome); // Call hackerEffect when the page loads
+    showRelevantPage(0);
+
+    // showRelevantPage(1);
 });
 
 const letters =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
 
-let isRunning = false;
+let isRunningHackerEffect = false;
 
 function hackerEffect(element) {
-    if (isRunning) {
+    if (isRunningHackerEffect) {
         return;
     }
-    isRunning = true;
+    isRunningHackerEffect = true;
     let iterations = 0;
     const interval = setInterval(() => {
         element.innerText = element.innerText.split("")
@@ -26,10 +29,61 @@ function hackerEffect(element) {
             .join("");
         if(iterations >= element.dataset.value.length){
             clearInterval(interval);
-            isRunning = false;
+            isRunningHackerEffect = false;
         } 
-        iterations += 1 / 8;
-    }, 40);
+        iterations += 1 / 12;
+    }, 30);
 }
 
 document.querySelector("#welcome").onmouseover = event => hackerEffect(event.target);
+
+
+const pages = ['#page1','#page2'];
+
+function showRelevantPage(pageNum) {
+    pages.forEach(page => {
+        document.querySelector(page).style.display = 'none';
+    })
+    document.querySelector(pages[pageNum]).style.display = 'block';
+}
+
+document.querySelector('#next').onclick = (event) => {
+    const element = event.target.parentElement.parentElement;
+    element.style.animationPlayState = 'running';
+    element.addEventListener('animationend', (event) => {
+        if (event.animationName === 'fade-out-page') {
+            element.style.animationPlayState = 'paused';
+            showRelevantPage(1);
+            typingEffect(document.querySelector('#description-page2'));
+        }
+    })
+}
+
+document.querySelector('.goBackButton').onclick = () => {
+    isRunningHackerEffect = false;
+    hackerEffect(welcome);
+    showRelevantPage(0);
+}
+
+let isRunningTypingEffect = false;
+
+function typingEffect(element) {
+    if (isRunningTypingEffect) {
+        return;
+    }
+
+    isRunningTypingEffect = true;
+    let text = element.textContent;
+    let index = 0;
+    element.textContent = '';
+
+    let interval = setInterval(() => {
+        element.textContent += text[index];
+        index++;
+
+        if (index >= text.length) {
+            clearInterval(interval);
+            isRunningTypingEffect = false;
+        }
+    }, 60);
+}
